50cf6b25060b55873dd851dd32be30118dd2f462
Pedro Lucas <plsousasilva1000@gmail.com>
2025-11-03 14:22:33 -0300

    arquivos dao aluno e empresa

--- codigo/src/dao/AlunoDAO.js
+++ codigo/src/dao/AlunoDAO.js
@@ -1,26 +1,43 @@
+const { randomUUID } = require('crypto');
+
 class AlunoDAO {
   constructor(db) {
-    this.db = db; 
+    this.db = db; // espera um objeto com métodos: insert, findById, findAll, update, delete
+    this.collection = 'alunos';
   }
 
   async create(aluno) {
-    throw new Error('AlunoDAO.create não implementado');
+    const id = aluno.id || randomUUID();
+    const now = new Date().toISOString();
+    const record = Object.assign({
+      id,
+      nome: null,
+      cpf: null,
+      rg: null,
+      endereco: null,
+      curso: null,
+      createdAt: now
+    }, aluno);
+
+    await this.db.insert(this.collection, record);
+    return record;
   }
 
   async findById(id) {
-    throw new Error('AlunoDAO.findById não implementado');
+    return this.db.findById(this.collection, id);
   }
 
-  async findAll(filter) {
-    throw new Error('AlunoDAO.findAll não implementado');
+  async findAll(filter = {}) {
+    return this.db.findAll(this.collection, filter);
   }
 
   async update(id, updates) {
-    throw new Error('AlunoDAO.update não implementado');
+    const updated = await this.db.update(this.collection, id, updates);
+    return updated;
   }
 
   async delete(id) {
-    throw new Error('AlunoDAO.delete não implementado');
+    return this.db.delete(this.collection, id);
   }
 }
 
--- codigo/src/dao/EmpresaDAO.js
+++ codigo/src/dao/EmpresaDAO.js
@@ -1,26 +1,42 @@
+const { randomUUID } = require('crypto');
+
 class EmpresaDAO {
   constructor(db) {
     this.db = db;
+    this.collection = 'empresas';
   }
 
   async create(empresa) {
-    throw new Error('EmpresaDAO.create não implementado');
+    const id = empresa.id || randomUUID();
+    const now = new Date().toISOString();
+    const record = Object.assign({
+      id,
+      nome: null,
+      cnpj: null,
+      emailComercial: null,
+      endereco: null,
+      ativa: true,
+      createdAt: now
+    }, empresa);
+
+    await this.db.insert(this.collection, record);
+    return record;
   }
 
   async findById(id) {
-    throw new Error('EmpresaDAO.findById não implementado');
+    return this.db.findById(this.collection, id);
   }
 
-  async findAll(filter) {
-    throw new Error('EmpresaDAO.findAll não implementado');
+  async findAll(filter = {}) {
+    return this.db.findAll(this.collection, filter);
   }
 
   async update(id, updates) {
-    throw new Error('EmpresaDAO.update não implementado');
+    return this.db.update(this.collection, id, updates);
   }
 
   async delete(id) {
-    throw new Error('EmpresaDAO.delete não implementado');
+    return this.db.delete(this.collection, id);
   }
 }
 
